295be29c59fec40b3d027cde61445abf2081b330
Keshav <kmalik0907@gmail.com>
2023-03-03 22:42:29 +0530

    third

--- src/App/Store.js
+++
@@ -1,5 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import counterRedcuer from "/Users/keshavmalik/Desktop/Projects/Ecom1/shop/src/Features/CounterSlice.js";
-export const store = configureStore({
-  reducer: { counter: counterRedcuer },
-});
--- src/index.js
+++ src/index.js
@@ -3,15 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { Auth0Provider } from "@auth0/auth0-react";
-import { store } from "./App/Store";
 import { Provider } from "react-redux";
 
   
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={store}>
+
     <Auth0Provider domain="dev-xwoqghc1yzr0r25r.us.auth0.com" clientId="U0I5XzwJVXonUjyMJxsGoWJe7hRb0tQu" redirectUri={window.location.origin}>
       <App />
     </Auth0Provider>
-  </Provider>
+
 );
